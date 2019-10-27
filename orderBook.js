41d9acd90f822a3de49db44d12b4d59f93cd5e8b
Patrick O'Connor <poc.967@gmail.com>
2019-10-27 12:53:43 -0400

    fully functional prototype

--- orderBook.js
+++ orderBook.js
@@ -9,27 +9,30 @@ function filterMatch(existingBook, incomingOrder) {
     && (bookItem.price === incomingOrder.price))
 }
 
+function priceDecider(existingPrice, incomingPrice) {
+  const priceIndex = existingPrice - incomingPrice
+  if (priceIndex >= 0) {
+    return true
+  }
+}
 
 function reconcileOrder(existingBook, incomingOrder) {
   const filteredBook = findMatch(existingBook, incomingOrder)
 
-  if (filteredBook.length) {
+  if (filteredBook.length && priceDecider(filteredBook[0].price, incomingOrder.price)) {
 
+    let updatedBook = filterMatch(existingBook, incomingOrder)
 
-    if ((filteredBook[0].quantity !== incomingOrder.quantity)) {
-      let updatedBook = filterMatch(existingBook, incomingOrder)
+    const updatedOrder = {
+      type: ((filteredBook[0].quantity > incomingOrder.quantity) ? filteredBook[0].type : incomingOrder.type),
+      quantity: Math.abs(filteredBook[0].quantity - incomingOrder.quantity),
+      price: ((filteredBook[0].price > incomingOrder.price) ? filteredBook[0].price : incomingOrder.price)
+    }
 
-      const updatedOrder = {
-        type: ((filteredBook[0].quantity > incomingOrder.quantity) ? filteredBook[0].type : incomingOrder.type),
-        quantity: Math.abs(filteredBook[0].quantity - incomingOrder.quantity),
-        price: ((filteredBook[0].price > incomingOrder.price) ? filteredBook[0].price : incomingOrder.price)
-      }
-
-      updatedBook = [...updatedBook, updatedOrder]
+    if (updatedOrder.quantity === 0) {
       return updatedBook
-
     } else {
-      let updatedBook = filterMatch(existingBook, incomingOrder)
+      updatedBook = [...updatedBook, updatedOrder]
       return updatedBook
     }
 
